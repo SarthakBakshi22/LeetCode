c4ea16a4efcc9c6948f8f2b8ed00bfe26d68d9fe
Sarthak Bakshi <48996741+SarthakBakshi22@users.noreply.github.com>
2023-06-30 23:23:27 -0400

    Create longCommSubSeq.py

---
+++ Blind75/1143 - Longest Common Subsequence/longCommSubSeq.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # currInd=0
+        # prevInd=0
+        # count=0
+        # if text1==text2:
+        #     return len(text1)
+        # if len(text1)>len(text2):
+        #     maxStr=text1
+        #     minStr=text2
+        # else:
+        #     maxStr=text2
+        #     minStr=text1
+        # print(minStr)
+        # for ch in minStr:
+        #     if ch in maxStr:
+        #         currInd=maxStr.index(ch)
+        #         if currInd >= prevInd:
+        #             prevInd=currInd
+        #             count+=1
+        # return count
+        dp=[[0 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                if text1[i]==text2[j]:
+                    dp[i][j]=1+dp[i+1][j+1]
+                else:
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1])
+        return dp[0][0]
